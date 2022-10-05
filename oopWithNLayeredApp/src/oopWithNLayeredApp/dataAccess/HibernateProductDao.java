6a7028d9b03178ae822b3a37bde7b4281e5f04cd
ugurcansezgin <87565365+ugurcansezgin@users.noreply.github.com>
2022-10-05 23:51:25 +0300

    Add files via upload

---
+++ oopWithNLayeredApp/src/oopWithNLayeredApp/dataAccess/HibernateProductDao.java
@@ -0,0 +1,16 @@
+package oopWithNLayeredApp.dataAccess;
+
+import oopWithNLayeredApp.entities.Product;
+
+public class HibernateProductDao implements ProductDao {
+
+	public void add(Product product) {
+		
+		//Sadece ve sadece db erişim kodları buraya yazılır.
+		
+		System.out.println("Hibernate ile veritabanına eklendi.");
+		
+	}
+}
+
+ 
