6ed3f3e21c29e12d31692f6252cceaf131f87e53
ugurcansezgin <87565365+ugurcansezgin@users.noreply.github.com>
2022-11-03 02:45:04 +0300

    PostgreSQL

---
+++ campsping2/rentACar/src/main/java/kodlama/io/rentACar/business/requests/CreateBrandRequest.java
@@ -0,0 +1,22 @@
+package kodlama.io.rentACar.business.requests;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateBrandRequest {
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
---
+++ campsping2/rentACar/src/main/java/kodlama/io/rentACar/business/responses/GetAllBrandsResponse.java
@@ -0,0 +1,28 @@
+package kodlama.io.rentACar.business.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetAllBrandsResponse {
+	
+	private int id;
+	private String name;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
